37f0b2c012acb42949f7e20517e97b2fad24bf66
Dmitriy Dubson <d.dubson@gmail.com>
2019-04-12 14:08:14 -0400

    Initial commit

--- src/AppRoot.jsx
+++ src/AppRoot.jsx
@@ -3,7 +3,20 @@ import React, {PureComponent} from "react";
 export class AppRoot extends PureComponent {
     render() {
         return (
-            <div>Hello World!</div>
+            <div className="container">
+                <nav>
+
+                </nav>
+
+                <section>
+                    <section>
+                        <div>Java</div>
+                        <div>
+                            Hello World
+                        </div>
+                    </section>
+                </section>
+            </div>
         )
     }
 }
