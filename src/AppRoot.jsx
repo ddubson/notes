2c2d3366ffbf156ac3d035b3b52e2d32e8ebf26e
Dmitriy Dubson <d.dubson@gmail.com>
2019-04-12 15:13:20 -0400

    Add more stuff

--- src/AppRoot.jsx
+++ src/AppRoot.jsx
@@ -1,22 +1,23 @@
-import React, {PureComponent} from "react";
+import React, { PureComponent } from "react";
 
 export class AppRoot extends PureComponent {
-    render() {
-        return (
-            <div className="container">
-                <nav>
-
-                </nav>
-
-                <section>
-                    <section>
-                        <div>Java</div>
-                        <div>
-                            Hello World
-                        </div>
-                    </section>
-                </section>
+  render() {
+    return (
+      <React.Fragment>
+        <nav className="navbar navbar-fixed-top">
+          Engineering Notes
+        </nav>
+        <div className="container">
+          <section className="note-section">
+            <h5>Java</h5>
+            <div className="note-section-body">
+              <ul>
+                <li>Flatten a list of lists</li>
+              </ul>
             </div>
-        )
-    }
+          </section>
+        </div>
+      </React.Fragment>
+    )
+  }
 }
--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import {AppRoot} from "./AppRoot";
 
+import "./styles/basic/basic-theme.scss"
+
 ReactDOM.render(
     <AppRoot />,
     document.getElementById("root"),
